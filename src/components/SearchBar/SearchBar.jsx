cf7aa547eb75a7c24160b91bef3b822c5883b829
Nazar-Kotiv <kotnazar20@gmail.com>
2024-02-26 11:14:39 +0100

    commit

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,5 @@
 import css from "./SearchBar.module.css";
+import { FaSearch } from "react-icons/fa";
 import toast, { Toaster } from "react-hot-toast";
 export default function SearchBar({ onSubmit }) {
   const handleSubmit = (e) => {
@@ -14,8 +15,10 @@ export default function SearchBar({ onSubmit }) {
   };
   return (
     <header className={css.header}>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={css.form}>
+        <FaSearch className={css.icon} />
         <input
+          className={css.inputSearch}
           type="text"
           name="submit"
           autoComplete="off"
